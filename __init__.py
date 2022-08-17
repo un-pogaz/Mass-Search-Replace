aa13480c943b62534ca13d96fd72e474955e9759
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2022-08-17 19:21:32 +0200

    imporve get_plugin_attribut

--- __init__.py
+++ __init__.py
@@ -17,7 +17,6 @@ except NameError:
 # The class that all Interface Action plugin wrappers must inherit from
 from calibre.customize import InterfaceActionBase
 
-DEBUG_PRE = 'MassSearchReplace'
 
 class ActionMassSearchReplace(InterfaceActionBase):
     '''
@@ -25,7 +24,7 @@ class ActionMassSearchReplace(InterfaceActionBase):
     plugin class. The actual interface plugin class is called InterfacePlugin
     and is defined in the ui.py file, as specified in the actual_plugin field
     below.
-
+    
     The reason for having two classes is that it allows the command line
     calibre utilities to run without needing to load the GUI libraries.
     '''
@@ -35,35 +34,37 @@ class ActionMassSearchReplace(InterfaceActionBase):
     author                  = 'un_pogaz'
     version                 = (1, 5, 0)
     minimum_calibre_version = (4, 0, 0)
-
+    
     #: This field defines the GUI plugin class that contains all the code
     #: that actually does something. Its format is module_path:class_name
     #: The specified class must be defined in the specified module.
-    actual_plugin           = 'calibre_plugins.mass_search_replace.action:MassSearchReplaceAction'
-
+    actual_plugin           = __name__+'.action:MassSearchReplaceAction'
+    
+    DEBUG_PRE = 'MassSearchReplace'
+    
     def is_customizable(self):
         '''
         This method must return True to enable customization via
         Preferences->Plugins
         '''
         return True
-
-
+    
+    
     def config_widget(self):
         '''
         Implement this method and :meth:`save_settings` in your plugin to
         use a custom configuration dialog.
-
+        
         This method, if implemented, must return a QWidget. The widget can have
         an optional method validate() that takes no arguments and is called
         immediately after the user clicks OK. Changes are applied if and only
         if the method returns True.
-
+        
         If for some reason you cannot perform the configuration at this time,
         return a tuple of two strings (message, details), these will be
         displayed as a warning dialog to the user and the process will be
         aborted.
-
+        
         The base class implementation of this method raises NotImplementedError
         so by default no user configuration is possible.
         '''
@@ -74,11 +75,11 @@ class ActionMassSearchReplace(InterfaceActionBase):
         if self.actual_plugin_:
             from calibre_plugins.mass_search_replace.config import ConfigWidget
             return ConfigWidget(self.actual_plugin_)
-
+    
     def save_settings(self, config_widget):
         '''
         Save the settings specified by the user with config_widget.
-
+        
         :param config_widget: The widget returned by :meth:`config_widget`.
         '''
         config_widget.save_settings()
